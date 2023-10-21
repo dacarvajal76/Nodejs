11fe914a199f0e560de49b5f23d6f778b0fb0bb2
dacarvajal.76 <dacarvajal.76@gmail.com>
2023-10-21 12:21:38 -0400

    Adding findUser correct format

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -4,19 +4,47 @@ exports.pong = (req,res) => {
   res.send("PONG")
 }
  
+// Controlador
 exports.findUsers = (req, res) => {
+  const page = parseInt(req.query.page) || 1; // Obtener el número de página de la consulta (por defecto es 1)
+  const limit = parseInt(req.query.limit) || 10; // Obtener el límite de elementos por página de la consulta (por defecto es 10)
+
+  // Calcular el índice de inicio y fin para la paginación
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+
+  // Definir los enlaces a la página siguiente y previa
+  const baseUrl = req.protocol + "://" + req.get("host") + req.originalUrl.split("?")[0]; // Obtener la URL base sin los parámetros de consulta
+  const totalItems = User.countDocuments(); // Obtener el número total de objetos
+  const totalPages = Math.ceil(totalItems / limit);
+
+  const pagination = {
+    totalItems: totalItems,
+    totalPages: totalPages,
+    currentPage: page,
+  };
+
+  // Ejecutar la consulta para obtener los objetos
   User.find()
+    .skip(startIndex)
+    .limit(limit)
     .then((data) => {
-      res.send(data);
+      // Construir el objeto de respuesta con el formato deseado
+      const response = {
+        count: data.length,
+        next: page < totalPages ? `${baseUrl}?page=${page + 1}&limit=${limit}` : null,
+        previous: page > 1 ? `${baseUrl}?page=${page - 1}&limit=${limit}` : null,
+        results: data,
+      };
+
+      res.send(response);
     })
     .catch((err) => {
       res.status(500).send({
-        message:
-          err.message || "Some error occurred while retrieving Users.",
+        message: err.message || "Some error occurred while retrieving Users.",
       });
     });
 };
-
 
 
 exports.createNewUser = (req, res) => {
